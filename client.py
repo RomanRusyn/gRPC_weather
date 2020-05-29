469292df32e0b14e639dcecccc7d3714ecbb2541
Roman <romanR@ss>
2020-05-29 22:04:57 +0300

    "grpc_message":"Exception calling application: Value must be iterable","grpc_status":2}"

--- client.py
+++ client.py
@@ -12,12 +12,20 @@ stub = weather_pb2_grpc.WeatherAppStub(channel)
 
 # create a valid request message
 city = weather_pb2.City(name="Rivne")
+print(f"city = {city}")
+print(f"stub-{stub}")
 # weather = weather_pb2.Weather(conditions="cloudy",temp=12,humidity=222,
 #                               pressure=1080,timestamp=1242335)
 
 # make the call
 # response = stub.GetWeather(city)
-response = weather_pb2.CityWeather()
+# response = weather_pb2.CityWeather()
+# stub.GetWeather(city)
+response = stub.GetWeather(weather_pb2.City(name='Rivne'))
+print(f"citiweather_client: {type(response)}")
+print(f"citiweather_client: {type(weather_pb2.Weather)}")
+print(f"citiweather_client: {type(response.weather)}")
+print(f"citiweather_client type is: {type(weather_pb2.CityWeather())}")
 
 
 print(response)
--- server.py
+++ server.py
@@ -1,13 +1,15 @@
-import grpc
-from concurrent import futures
 import time
+from concurrent import futures
 
+# import the original consumer.py
+import consumer
+import grpc
 # import the generated classes
 import weather_pb2
 import weather_pb2_grpc
 
-# import the original consumer.py
-import consumer
+
+# import test_consumer
 
 # create a class to define the server functions, derived from
 # weather_pb2_grpc.CalculatorServicer
@@ -24,13 +26,34 @@ class WeatherAppServicer(weather_pb2_grpc.WeatherAppServicer):
 
     def GetWeather(self, request, context):
         request_weather = request.name
+        print(f"request_weather from GetWeather = {request_weather}")
+        print(f"reauest: {request}")
 
         # text = request_weather.replace('\n', '').replace('\r', '')
         # words = text.split(' ')
         # keywords = self.keywords(words)
+        # weather = weather_pb2.Weather(conditions="cloudy", temp=12,
+        #                               humidity=222,
+        #                               pressure=1080, timestamp=1242335)
+        # weather2 = weather_pb2.Weather(["cloudy", 12, 222, 1080, 1235453])
+        print("_____________before weather__________-")
+        cityweather1 = weather_pb2.CityWeather()
+        weather1 = cityweather1.weather.add()
+        weather1.conditions="conditions"
+        weather1.temp = 12
+        weather1.humidity = 123
+        weather1.pressure = 1080
+        weather1.timestamp = 1234456
+        # weather3 = weather_pb2.Weather.extend(
+        #     ["cloudy", 12.0, 222.9, 1080.6, 1235453])
+        # w4= weather_pb2.CityWeather
+        print("_________after weather------------")
 
-        
-        response = weather_pb2.CityWeather()
+        city1 = weather_pb2.City(name="Rivne")
+        print("--------before response")
+        response = weather_pb2.CityWeather(city=city1, weather=weather1)
+        # response = request_weather
+        print(f"response is: {response}")
         # response.keywords.extend(keywords)
 
         return response
@@ -48,6 +71,7 @@ def serve():
     # listen on port 50051
     print('Starting server. Listening on port 50051.')
     server.add_insecure_port('[::]:50051')
+
     server.start()
 
     # since server.start() will not block,
@@ -59,7 +83,5 @@ def serve():
         server.stop(0)
 
 
-
-
-if __name__== "__main__":
+if __name__ == "__main__":
     serve()
